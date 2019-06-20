0778402e5ad0db0744fee170ac4fb84a0e87cf02
Simon Franzen <simon@zauberware.com>
2019-06-20 14:32:55 +0200

    config update

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,11 @@ module RailsDeviseGraphql
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.autoload_paths << Rails.root.join("lib")
+    config.eager_load_paths << Rails.root.join("lib")
+
+    # config.autoload_paths += Dir[Rails.root.join('app', 'graphql')]
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,9 +30,19 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.perform_deliveries = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              ENV['SMTP_ADDRESS'],
+    port:                 ENV['SMTP_PORT'].to_i,
+    domain:               ENV['SMTP_DOMAIN'],
+    user_name:            ENV['SMTP_USERNAME'],
+    password:             ENV['SMTP_PASSWORD'],
+    authentication:       ENV['SMTP_AUTH'],
+    enable_starttls_auto: ENV['SMTP_ENABLE_STARTTLS_AUTO'] == "true"
+  }
 
   config.action_mailer.perform_caching = false
 
