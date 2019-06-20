78cf593fd208db2e1a1f530a9d352b61b009fb83
Simon Franzen <simon@zauberware.com>
2019-06-20 14:28:06 +0200

    Add admin user to env vars

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-User.create!(
-  email: "demo@zauberware.com",
-  password: "demo123",
-  password_confirmation: "demo123",
-  first_name: "John",
-  last_name: "Doe"
+User.create(
+  email: ENV['ADMIN_EMAIL'],
+  password: ENV['ADMIN_PASSWORD'],
+  password_confirmation: ENV['ADMIN_PASSWORD'],
+  first_name: ENV['ADMIN_FIRST_NAME'],
+  last_name: ENV['ADMIN_LAST_NAME']
 )
