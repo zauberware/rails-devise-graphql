305c047f5750ad0cc6fc3083ba93e16c24d6a27e
Simon Franzen <simon@zauberware.com>
2020-09-12 17:25:55 +0200

    Split up i18n files

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -5,3 +5,5 @@ I18n.available_locales = %i[en de]
 
 # Set default locale to something other than :en
 I18n.default_locale = :en
+
+I18n.load_path += Dir[Rails.root.join('config/locales/**/*yml').to_s]
