edcec4e6964c2b50f9f9e452f54b36ab86a6b067
Simon Franzen <simon@zauberware.com>
2020-09-12 11:37:53 +0200

    better graphql error handling

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -18,9 +18,7 @@ class GraphqlController < ApplicationController
     )
     render json: result
   rescue StandardError => e
-    raise e unless Rails.env.development?
-
-    handle_error_in_development e
+    handle_error e
   end
 
   private
@@ -47,15 +45,14 @@ class GraphqlController < ApplicationController
     end
   end
 
-  def handle_error_in_development(err)
+  def handle_error(err)
     logger.error err.message
     logger.error err.backtrace.join("\n")
 
     render json: {
-      error: {
-        message: err.message,
-        backtrace: err.backtrace
-      },
+      errors: [
+        { message: err.message }
+      ],
       data: {}
     }, status: 500
   end
