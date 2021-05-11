54f672a902853064ff94d10b284badeca7b45eab
Jonathan Graham <sendthistojon@gmail.com>
2021-05-11 10:37:36 -0400

    upgrade to Ruby 3.0.1 with related gem updates

--- lib/regex.rb
+++ lib/regex.rb
@@ -2,7 +2,7 @@
 
 module Regex
   class Email
-    VALIDATE = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i.freeze
-    VALIDATE_OR_EMPTY = /(^$|#{VALIDATE})/i.freeze
+    VALIDATE = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+    VALIDATE_OR_EMPTY = /(^$|#{VALIDATE})/i
   end
 end
